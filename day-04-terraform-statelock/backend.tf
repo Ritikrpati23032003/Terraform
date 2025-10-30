ecefcf0b38efef05f0dbb54740ddff644ff7e8a0
Ritik Ranja Pati <ritikranjanpati@gmail.com>
2025-10-30 16:34:22 +0530

    lock2

--- day-04-terraform-statelock/backend.tf
+++ day-04-terraform-statelock/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket = "terraform-29-10-25"
     key    = "day4/terraform.tfstate"
-    use_lockfile = true # to use s3 native locking 1.19 version above
     region = "us-east-1"
+    #use_lockfile = true # to use s3 native locking 1.19 version above
+    dynamodb_table = "terraform" # any version we can use dynamodb locking 
+    encrypt = true
   }
 }
--- day-04-terraform-statelock/main.tf
+++ day-04-terraform-statelock/main.tf
@@ -1,12 +1,11 @@
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
-      Name= "ritik"
+      Name= "DEV"
     }
   }
 
   resource "aws_subnet" "name" {
-
   vpc_id     = aws_vpc.name.id
   cidr_block = "10.0.0.0/24"
   tags = {
