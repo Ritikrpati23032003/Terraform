2877069dabf6eb2fec5eebb11af3964b3bb9dfd2
Ritik Ranja Pati <ritikranjanpati@gmail.com>
2025-10-30 16:17:57 +0530

    lock

--- day-04-terraform-statelock/main.tf
+++ day-04-terraform-statelock/main.tf
@@ -1,6 +1,13 @@
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
-      Name= "DEV"
+      Name= "ritik"
     }
   }
+  resource "aws_subnet" "name" {
+  vpc_id     = aws_vpc.name.id
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "Subnet-01"
+  }
+}
